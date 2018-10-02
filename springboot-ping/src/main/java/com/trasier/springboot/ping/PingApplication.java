a7b637a2164f388d813e4ab60f0e045d80b8348d
codepfleger <mail@codepfleger.de>
2018-10-02 20:39:57 +0200

    trasier

--- springboot-ping/src/main/java/com/trasier/springboot/ping/PingApplication.java
+++ springboot-ping/src/main/java/com/trasier/springboot/ping/PingApplication.java
@@ -1,14 +1,11 @@
 package com.trasier.springboot.ping;
 
-import com.trasier.client.impl.spring.opentracing.boot.EnableTrasierOpentracing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableTrasierOpentracing
 public class PingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PingApplication.class, args);
+    }
 }
--- springboot-pong/src/main/java/com/trasier/springboot/pong/PongApplication.java
+++ springboot-pong/src/main/java/com/trasier/springboot/pong/PongApplication.java
@@ -1,14 +1,11 @@
 package com.trasier.springboot.pong;
 
-import com.trasier.client.impl.spring.opentracing.boot.EnableTrasierOpentracing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableTrasierOpentracing
 public class PongApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PongApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PongApplication.class, args);
+    }
 }
