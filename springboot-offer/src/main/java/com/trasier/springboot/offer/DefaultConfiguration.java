8c10fc15700ade03e0a64b23666d17569bd60bd2
codepfleger <mail@codepfleger.de>
2018-10-02 21:57:25 +0200

    zipkin

--- springboot-offer/src/main/java/com/trasier/springboot/offer/DefaultConfiguration.java
+++ springboot-offer/src/main/java/com/trasier/springboot/offer/DefaultConfiguration.java
@@ -1,13 +1,26 @@
 package com.trasier.springboot.offer;
 
+import brave.Tracing;
+import brave.opentracing.BraveTracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.okhttp3.OkHttpSender;
 
 @Configuration
 public class DefaultConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    } 
+    }
+
+    @Bean
+    public io.opentracing.Tracer zipkinTracer() {
+        OkHttpSender okHttpSender = OkHttpSender.create("http://localhost:9411/api/v2/spans");
+        AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
+        Tracing braveTracer = Tracing.newBuilder().localServiceName("offer").spanReporter(reporter).build();
+        return BraveTracer.create(braveTracer);
+    }
 }
