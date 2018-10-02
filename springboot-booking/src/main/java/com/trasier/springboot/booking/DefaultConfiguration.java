ebcbb0c13bd109adff1285a8a3b2ccacbafd714a
codepfleger <mail@codepfleger.de>
2018-10-02 21:16:22 +0200

    jaeger

--- springboot-booking/src/main/java/com/trasier/springboot/booking/DefaultConfiguration.java
+++ springboot-booking/src/main/java/com/trasier/springboot/booking/DefaultConfiguration.java
@@ -1,5 +1,6 @@
 package com.trasier.springboot.booking;
 
+import io.jaegertracing.samplers.ProbabilisticSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,5 +10,12 @@ public class DefaultConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    } 
+    }
+
+    @Bean
+    public io.opentracing.Tracer jaegerTracer() {
+        return new io.jaegertracing.Configuration("booking", new io.jaegertracing.Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+                new io.jaegertracing.Configuration.ReporterConfiguration())
+                .getTracer();
+    }
 }
