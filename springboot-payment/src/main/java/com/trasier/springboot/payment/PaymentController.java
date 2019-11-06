68eb5dc9b984ce3ba68000a08403c65473a9527f
codepfleger <mail@codepfleger.de>
2019-11-06 11:07:26 +0100

    timeout

--- springboot-payment/src/main/java/com/trasier/springboot/payment/PaymentController.java
+++ springboot-payment/src/main/java/com/trasier/springboot/payment/PaymentController.java
@@ -5,13 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class PaymentController {
     @GetMapping(value = "/payment/{offerId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String checkPayment(@PathVariable("offerId") String offerId) {
+    public String checkPayment(@PathVariable("offerId") String offerId) throws InterruptedException {
         if ("42".equals(offerId)) {
             return "{ \"offerId\": " + offerId + ", \"status\": \"PAID\" }";
         } else {
+            TimeUnit.SECONDS.sleep(5);
             return "{ \"offerId\": " + offerId + ", \"status\": \"DECLINED\" }";
         }
     }
